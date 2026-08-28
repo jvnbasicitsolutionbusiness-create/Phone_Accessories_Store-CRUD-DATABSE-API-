ed390d3ae24841fe3ca149c5b98ccd9826da16a1
jvnbasicitsolutionbusiness-create <jvnbasicitsolutionbusiness@gmail.com>
2026-08-28 13:28:18 +0800

    Update Firebase and Google Sheet API URLs

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const STOCKFLOW_CONFIG = {
-  FIREBASE_DATABASE_URL: "https://midtermexamproject-default-rtdb.firebaseio.com",
-  GOOGLE_SHEET_API_URL: "",
+  FIREBASE_DATABASE_URL: "https://midtermexamproject-default-rtdb.firebaseio.com/",
+  GOOGLE_SHEET_API_URL: "https://script.google.com/macros/s/AKfycbytfBA-SJDFkD8QlzHqpl65Lqg4CXkLfAZV2vec1Y36RcuIKbcwOER8jgDhIDeHtlgefw/exec",
   ENABLE_GOOGLE_SHEET_SYNC: false
 };
 window.STOCKFLOW_CONFIG = STOCKFLOW_CONFIG;
